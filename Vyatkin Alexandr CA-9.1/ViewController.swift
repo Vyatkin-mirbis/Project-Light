d3648cb730b35a1c7c16165d31e24004e1e85f02
student <student@productservice.ttknn.net>
2017-12-08 11:43:34 +0300

    Initial Commit

---
+++ Vyatkin Alexandr CA-9.1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vyatkin Alexandr CA-9.1
+//
+//  Created by student on 08.12.2017.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
